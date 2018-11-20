3fab8a2e29343d3332f543867e8bffcc1b294ca8
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-11-20 11:21:26 -0800

    Create session controller and view pages for auth

--- RedditOnRails/app/controllers/sessions_controller.rb
+++ RedditOnRails/app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if user
+      login(user)
+      render json: user
+    else
+      flash[:errors] = ["Invalid Credentials"]
+      redirect_to new_session_url
+    end
+  end
+
+  def destroy
+    logout!
+    redirect_to new_session_url
+  end
 end
