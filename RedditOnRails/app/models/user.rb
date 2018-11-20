40bee042ca23cf6b904cab4d2067c394d675a7ae
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-11-20 11:01:52 -0800

    Create User table, make user model and controller for auth

---
+++ RedditOnRails/app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+  before_validation :ensure_session_token
+
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: {message: 'Password cannot be empty'}
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  attr_reader :password
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+end
